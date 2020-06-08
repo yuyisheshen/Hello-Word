42a972dd4b9f3146d88140198fcdf0c8c1381b14
yuyisheshen <63548600+yuyisheshen@users.noreply.github.com>
2020-06-09 00:05:02 +0800

    Create HelloWord.java
    这是我的愿景

---
+++ HelloWord.java
@@ -0,0 +1,6 @@
+public static class HelloWord{
+  public static void main(String args){
+  System.out.println{"Hello Word!"};
+  System.out.println{"My name is wings snake god,I have suffed a lot,but I am strong,I belive everything will be better!"};
+  }
+}
